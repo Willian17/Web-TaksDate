30aa37f7f6e7874c4d37e4a2ebb29345241dc86d
Willian17 <willianfernandes113@gmail.com>
2020-10-05 15:14:37 -0400

    delivery date

--- src/components/TaskItem/index.tsx
+++ src/components/TaskItem/index.tsx
@@ -10,18 +10,12 @@ export interface TaskItemProps {
     deliverydate: string
 }
 const TaskItem: React.FC<TaskItemProps> = ( {title, subject, deliverydate} ) => {
-    console.log('deliverydate da API'  + deliverydate)
-    const deliveryDate = new Date(deliverydate)
-    console.log('deliveryDate do service'  + deliveryDate)
 
-
-    const deliverydateFormated = format(deliveryDate, "yyyy/MM/dd HH:mm")
-    console.log('deliveryDate do service formated'  + deliverydateFormated)
 
     return (
        <TaskItemContainer>
            <h1>{title} de {subject}</h1>
-           <h3>{deliverydateFormated}</h3>
+           <h3>{deliverydate}</h3>
        </TaskItemContainer>
     )
 }
