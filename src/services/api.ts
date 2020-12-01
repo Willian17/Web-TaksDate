ec422747d0d3f6fe50d17bec4df5d4538b517be9
Willian17 <willianfernandes113@gmail.com>
2020-12-01 10:34:45 -0400

    deploy

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,6 +2,7 @@ import axios from 'axios'
 import { getToken } from '../utils/cookie'
 const API_URL = process.env.NODE_ENV == 'development' ? 'http://localhost:3333': 'https://tasksdate.herokuapp.com'
 const token = getToken();
+console.log(token);
 
 const api = axios.create({
     baseURL: API_URL, 
